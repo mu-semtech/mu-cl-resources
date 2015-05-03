be72eac8edd8b7374d24706f5fc5214940267206
Aad Versteden <madnificent@gmail.com>
2015-05-03 18:49:53 +0200

    Support and implement update call
    With this commit we have basic suppor for update calls.  The update is
    always the full object, which may not be exactly what we want.  Also,
    when something goes haywire, the original content is removed, rather
    than us keeping the old content around.  We shouldfix these problems in
    the near future.

--- domain.lisp
+++ domain.lisp
@@ -23,29 +23,72 @@
        :ld-properties ,properties
        :ld-resource-base ,resource-base))
 
+(defun property-paths-format-component (resource)
+  (declare (ignore resource))
+  "~{~&~4t~{~A~,^/~} \"~A\"~,^;~}.")
+(defun property-paths-content-component (resource json-input)
+  (loop for (property . path)
+     in (ld-properties resource)
+     append (list (mapcar #'clean-url path)
+                  (jsown:filter json-input
+                                "data"
+                                (string-downcase
+                                 (string property))))))
+
 (defgeneric create-call (resource)
   (:documentation "implementation of the POST request which
     handles the creation of a resource.")
   (:method ((resource-symbol symbol))
     (create-call (gethash resource-symbol *resources*)))
   (:method ((resource resource))
-    (let ((json-input (jsown:parse (post-body))))
+    (let ((json-input (jsown:parse (post-body)))
+          (uuid (princ-to-string (uuid:make-v4-uuid))))
       (insert *repository* ()
         (s+
          "GRAPH <http://mu.semte.ch/application/> { "
-         "  <~A> a <~A>;~{~&~4t~{<~A>~,^/~} \"~A\"~,^;~}."
+         "  <~A> a <~A>;"
+         "  ~&~4tmu:uuid \"~A\";"
+         (property-paths-format-component resource)
          "}")
-        (clean-url (s+ (ld-resource-base resource)
-                       (princ-to-string (uuid:make-v4-uuid))))
+        (clean-url (s+ (ld-resource-base resource) uuid))
         (clean-url (ld-class resource))
-        (loop for (property . path)
-           in (ld-properties resource)
-           append (list (mapcar #'clean-url path)
-                        (jsown:filter json-input
-                                      "data"
-                                      (string-downcase
-                                       (string property)))))))
+        uuid
+        (property-paths-content-component resource json-input)))
     (jsown:new-js ("success" :true))))
+
+(defgeneric update-call (resource uuid)
+  (:documentation "implementation of the PUT request which
+    handles the updating of a resource.")
+  (:method ((resource-symbol symbol) uuid)
+    (update-call (gethash resource-symbol *resources*) uuid))
+  (:method ((resource resource) (uuid string))
+    ;; ideally, we'd be a lot more prudent with deleting content
+    (let ((json-input (jsown:parse (post-body))))
+      (fuseki:query
+       *repository*
+       (format nil
+               (s+
+                "DELETE WHERE {"
+                "  GRAPH <http://mu.semte.ch/application/> { "
+                "    ?s mu:uuid \"~A\"; "
+                "    ~{~&~8t~{~A~,^/~} ?~A~,^;~}."
+                "  }"
+                "}")
+               (clean-string uuid)
+               (loop for (property . path)
+                  in (ld-properties resource)
+                  for i from 0
+                  append (list (mapcar #'clean-url path)
+                               (format nil "gensym~A" i)))))
+      (insert *repository* ()
+        (s+
+         "GRAPH <http://mu.semte.ch/application/> { "
+         "  <~A> mu:uuid \"~A\"; "
+         (property-paths-format-component resource)
+         "}")
+        (clean-url (s+ (ld-resource-base resource) (clean-string uuid)))
+        (clean-string uuid)
+        (property-paths-content-component resource json-input)))))
 
 (define-resource product-groups ()
   :class "http://veeakker.com/vocabulary/shop/ProductGroup"
@@ -59,6 +102,8 @@
 ;;;; GET request
 
 ;;;; PUT request
+(defcall :put (:product-groups uuid)
+  (update-call 'product-groups uuid))
 
 ;;;; POST request
 ;; create a new resource
