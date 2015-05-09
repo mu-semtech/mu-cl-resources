a975effcba8d0521de4fb9feac79f6a098ac5060
Aad Versteden <madnificent@gmail.com>
2015-05-09 16:17:20 +0200

    Define our resource name in singular
    The resource name should be a singular entity so the top-level type
    attributes is used correctly.  It also makes more sense to define the
    singular resource in a non-pluralized key.

--- domain.lisp
+++ domain.lisp
@@ -188,7 +188,7 @@
                                              #'string)
                                             property)))))))
 
-(define-resource product-groups ()
+(define-resource product-group ()
   :class (s-url "http://veeakker.com/vocabulary/shop/ProductGroup")
   :properties `((:name ,(s-prefix "productGroup:name"))
                 (:color ,(s-prefix "productGroup:color"))
@@ -196,24 +196,23 @@
   :resource-base (s-url "http://veeakker.com/api/product-groups/"))
 
 
-
 ;;;; LIST request
 (defcall :get (:product-groups)
-  (list-call 'product-groups))
+  (list-call 'product-group))
 
 ;;;; GET request
 (defcall :get (:product-groups uuid)
-  (show-call 'product-groups uuid))
+  (show-call 'product-group uuid))
 
 ;;;; PUT request
 (defcall :put (:product-groups uuid)
-  (update-call 'product-groups uuid))
+  (update-call 'product-group uuid))
 
 ;;;; POST request
 ;; create a new resource
 (defcall :post (:product-groups)
-  (create-call 'product-groups))
+  (create-call 'product-group))
 
 ;;;; DELETE request
 (defcall :delete (:product-groups uuid)
-  (delete-call 'product-groups uuid))
+  (delete-call 'product-group uuid))
