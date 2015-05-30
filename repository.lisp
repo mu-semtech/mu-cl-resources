a9d4ad06ea51b77f89e0f5d79b11dc9d9f28b3f1
Aad Versteden <madnificent@gmail.com>
2015-05-30 14:50:49 +0200

    Add prefix for product and productGroup
    We use these as the prefixes for the specified domain.

--- repository.lisp
+++ repository.lisp
@@ -1,3 +1,5 @@
 (in-package :product-groups)
 
 (add-prefix "productGroup" "http://veeakker.com/vocabulary/shop/product-groups/")
+(add-prefix "product" "http://veeakker.com/vocabulary/shop/product/")
+(add-prefix "producer" "http://veeakker.com/vocabulary/shop/producer/")
