f0230f450663d0229a078f05b4d61929555a6ce3
Aad Versteden <madnificent@gmail.com>
2015-07-18 16:05:13 +0200

    Update domain and author to include blog example
    One of the tiny projects we are building is a blogging site.  The
    example is now described in the new domain file.

--- repository.lisp
+++ repository.lisp
@@ -3,3 +3,6 @@
 (add-prefix "productGroup" "http://veeakker.com/vocabulary/shop/product-groups/")
 (add-prefix "product" "http://veeakker.com/vocabulary/shop/product/")
 (add-prefix "producer" "http://veeakker.com/vocabulary/shop/producer/")
+
+(add-prefix "ed" "http://ed.semte.ch/")
+(add-prefix "site" "http://mu.semte.ch/vocabularies/site/")
