af33d9b7f86801afc27a7359dd050cc2cee9b99a
Aad Versteden <madnificent@gmail.com>
2016-02-17 15:38:32 +0100

    Moving configuration files to configuration
    This will allow us to only overwrite the files of the configuration, not
    the files of the base system.  The options for extending will be similar
    to what they were before, but the chance for accidental errors becomes
    smaller.

--- configuration/repository.lisp
+++ configuration/repository.lisp
@@ -9,3 +9,14 @@
 ;; (add-prefix "bravoer" "http://mu.semte.ch/vocabularies/bravoer/")
 
 ;; (add-prefix "mt" "http://sem.tenforce.com/vocabularies/mapping-pilot/")
+
+
+(add-prefix "dcat" "http://www.w3.org/ns/dcat#")
+(add-prefix "dct" "http://purl.org/dc/terms/")
+(add-prefix "dctype" "http://purl.org/dc/dcmitype/")
+(add-prefix "foaf" "http://xmlns.com/foaf/0.1/")
+(add-prefix "rdf" "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+(add-prefix "rdfs" "http://www.w3.org/2000/01/rdf-schema#")
+(add-prefix "skos" "http://www.w3.org/2004/02/skos/core#")
+(add-prefix "vcard" "http://www.w3.org/2006/vcard/ns#")
+(add-prefix "xsd" "http://www.w3.org/2001/XMLSchema#")
--- load-config.sh
+++ load-config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp -R /config/* /app/framework/
+cp -R /config/* /app/configuration/
